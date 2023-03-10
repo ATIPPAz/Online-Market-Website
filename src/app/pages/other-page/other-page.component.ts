acefa5deb8932b89d12e2e8911974bb0b2c951eb
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-03-10 20:45:09 +0700

    Restuctor project

---
+++ src/app/pages/other-page/other-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-other-page',
+  templateUrl: './other-page.component.html',
+  styleUrls: ['./other-page.component.css'],
+})
+export class OtherPageComponentPage {}
