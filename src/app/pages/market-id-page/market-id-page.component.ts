c0c5766cd1ffdc81aff4532ac80a4256f0767c8d
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-04-09 11:37:58 +0700

    add path reader

--- src/app/pages/market-id-page/market-id-page.component.ts
+++ src/app/pages/market-id-page/market-id-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
 @Component({
   selector: 'app-market-id-page',
   templateUrl: './market-id-page.component.html',
-  styleUrls: ['./market-id-page.component.css']
+  styleUrls: ['./market-id-page.component.css'],
 })
 export class MarketIdPageComponent {
-
+  id: string
+  constructor(private routedata: ActivatedRoute, private route: Router) {}
+  ngOnInit(): void {
+    this.id = this.routedata.snapshot.paramMap.get('id')
+    console.log(this.id)
+  }
 }
