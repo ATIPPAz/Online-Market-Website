a460768bcad02626d81c576a4c796211525c2c0b
YasxxMiN <yasumin7742@gmail.com>
2023-04-18 03:27:40 +0700

    update Pro

--- src/app/pages/product-detail-page/product-detail-page.component.ts
+++ src/app/pages/product-detail-page/product-detail-page.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core'
   styleUrls: ['./product-detail-page.component.css'],
 })
 export class ProductDetailPageComponent {
-
+  value: number = 1
+  plus() {
+    this.value++
+  }
+  minus() {
+    if (this.value > 0) {
+      this.value--
+    }
+  }
 }
