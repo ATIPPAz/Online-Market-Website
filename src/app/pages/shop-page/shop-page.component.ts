860c9049cd0cfc43ae7b5a51e7a71cf801058eaf
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-04-18 13:06:25 +0700

    update catagory

--- src/app/pages/shop-page/shop-page.component.ts
+++ src/app/pages/shop-page/shop-page.component.ts
@@ -11,18 +11,19 @@ import { Shop } from '../../interfaces/shop'
   styleUrls: ['./shop-page.component.css'],
 })
 export class ShopPageComponent {
-  shopId: string
-  marketId: string
+  shopId: string= ''
+  marketId: string= ''
   shopData: Shop
-  shopName: string
-  catagoryId: string
-  catagoriesData: Catagory[]
-  productData: Product[]
+  shopName: string= ''
+  catagoryId: string = ''
+  catagoriesData: Catagory[] = []
+  productData: Product[]= []
   constructor(private routedata: ActivatedRoute, private api: ApiService) {}
   ngOnInit(): void {
     this.shopId = this.routedata.snapshot.paramMap.get('shopId')
     this.api.getOneShop(this.shopId).subscribe((e) => {
       this.shopData = e.data
+      this.shopName = this.shopData.shopName
     })
 
     this.api.getAllProduct(`shopId=${this.shopId}`).subscribe((e) => {
@@ -33,14 +34,20 @@ export class ShopPageComponent {
     })
   }
   getCatagory() {
-    return this.catagoriesData.forEach((e) => e.catagoryId == this.catagoryId)
+    const catagoty = this.catagoriesData.find((e) => e.catagoryId == this.catagoryId) 
+    return catagoty?catagoty.catagoryName:'All Catagory'
   }
-  fecthDataWithCatagory(id) {
+  hasProduct(){
+    return this.productData.length !== 0 ? true:false
+  }
+   fecthDataWithCatagory(id) {
     this.catagoryId = id
     this.api
       .getAllProduct(`shopId=${this.shopId}&catagoryId=${id}`)
       .subscribe((r) => {
         this.productData = r.data
       })
+      console.log(this.hasProduct());
+      
   }
 }
