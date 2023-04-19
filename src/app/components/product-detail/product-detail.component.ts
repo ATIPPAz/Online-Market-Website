aa8a73aa78d7a7c18e5afbab3ab20a17a841c517
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-04-19 22:45:45 +0700

    login require

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { Market } from '../../interfaces/market'
 import { Product } from '../../interfaces/product'
 import { Shop } from '../../interfaces/shop'
 import { Router } from '@angular/router'
+import { ApiService } from '../../services/api.service'
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
@@ -13,7 +14,7 @@ export class ProductDetailComponent {
   @Input('shop') shop: Shop
   @Input('product') product: Product
   qty: number = 1
-  constructor(private router: Router) {
+  constructor(private router: Router, private api: ApiService) {
 
   }
   ngOnInit(): void {
@@ -25,6 +26,12 @@ export class ProductDetailComponent {
     this.qty = qty
   }
   createOrder() {
-    this.router.navigate(['/order/product/' + this.product.productId])
+    if (this.api.Api().auth.isLogin()) {
+      this.router.navigate(['/order/product/' + this.product.productId])
+    } else {
+      alert('กรุณาไปล้อคอินเดี๋ยวนี้นะคะ !!')
+      this.router.navigate(['/login'])
+
+    }
   }
 }
