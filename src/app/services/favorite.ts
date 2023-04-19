a1ea3a54fe5c845bb0b3bb815f4ee18899337484
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-04-20 02:48:30 +0700

    can show fav

--- src/app/services/favorite.ts
+++ src/app/services/favorite.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs'
-
+import { Favorite } from '../interfaces/favorite'
 export class FavoriteApi {
   apiUr: string
   headers: HttpHeaders
@@ -13,6 +13,11 @@ export class FavoriteApi {
   liked({ shopId, productId }): Observable<{ status: number; data: any }> {
     return this._http.post<{ status: number; data: any }>(
       `${this.apiUr}/favorite`, { shopId, productId }
+      , { headers: this.headers })
+  }
+  getOne(): Observable<{ status: number; data: Favorite }> {
+    return this._http.get<{ status: number; data: Favorite }>(
+      `${this.apiUr}/favorite`
       , { headers: this.headers })
   }
 }
